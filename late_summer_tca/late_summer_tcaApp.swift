d8e47942b8b2f29f406b29310bd66b94c8980feb
Branislav Bilý <branislav.bily@livesport.eu>
2024-08-30 10:50:42 +0200

    Initial Commit

---
+++ late_summer_tca/late_summer_tcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  late_summer_tcaApp.swift
+//  late_summer_tca
+//
+//  Created by Branislav Bilý on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct late_summer_tcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ late_summer_tcaUITests/late_summer_tcaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  late_summer_tcaUITestsLaunchTests.swift
+//  late_summer_tcaUITests
+//
+//  Created by Branislav Bilý on 30.08.2024.
+//
+
+import XCTest
+
+final class late_summer_tcaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
