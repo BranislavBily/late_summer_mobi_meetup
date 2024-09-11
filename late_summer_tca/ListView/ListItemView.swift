bed1b0a37967309b869335f9bfffe8f8be1d3b94
Branislav Bilý <branislav.bily@livesport.eu>
2024-09-11 14:18:38 +0200

    Vacsi text

--- late_summer_tca/ListView/ListItemView.swift
+++ late_summer_tca/ListView/ListItemView.swift
@@ -7,12 +7,15 @@ struct ListItemView: View {
 	var body: some View {
 		HStack(spacing: 8) {
 			Image(systemName: model.type == .Player ? "person.fill" : "figure.2.and.child.holdinghands")
+				.resizable()
 				.frame(width: 24, height: 24)
 			Text(model.text)
+				.font(.title3)
+				.fixedSize()
 			Image(systemName: model.hearted ? "heart.fill" : "heart")
 				.frame(maxWidth: .infinity, alignment: .trailing)
 		}
-		.frame(maxWidth: .infinity, alignment: .leading)
+		.frame(height: 40)
 	}
 
 }
